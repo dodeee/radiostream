77efc08ccb47b933a943503805e409f2ebbd66dc
etienne <dode.etienne@gmail.com>
2019-10-13 16:41:50 +0200

    bd

--- dump_bd.sql
+++ dump_bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 27 sep. 2019 à 19:29
+-- Généré le :  Dim 13 oct. 2019 à 14:40
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -34,7 +34,14 @@ CREATE TABLE IF NOT EXISTS `directlive` (
   `son` varchar(200) NOT NULL,
   `heuredebut` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=605 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `directlive`
+--
+
+INSERT INTO `directlive` (`id`, `son`, `heuredebut`) VALUES
+(604, '0740.mp3', '2019-10-13 14:29:28');
 
 -- --------------------------------------------------------
 
@@ -48,7 +55,41 @@ CREATE TABLE IF NOT EXISTS `passee_ajd` (
   `son` varchar(150) NOT NULL,
   `jour` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=558 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `passee_ajd`
+--
+
+INSERT INTO `passee_ajd` (`id`, `son`, `jour`) VALUES
+(557, '0740.mp3', 285),
+(556, '2  La fÃªte est finie .mp3', 285);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `playlist`
+--
+
+DROP TABLE IF EXISTS `playlist`;
+CREATE TABLE IF NOT EXISTS `playlist` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(200) NOT NULL,
+  `heuredebut` int(2) NOT NULL DEFAULT '0',
+  `heurefin` int(2) NOT NULL DEFAULT '0',
+  `jourdebut` int(1) NOT NULL,
+  `jourfin` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `playlist`
+--
+
+INSERT INTO `playlist` (`id`, `nom`, `heuredebut`, `heurefin`, `jourdebut`, `jourfin`) VALUES
+(1, 'truc', 22, 2, 3, 4),
+(2, 'machin', 22, 3, 2, 3),
+(3, 'chose', 21, 23, 3, 3);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
